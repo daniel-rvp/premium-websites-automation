30a1d585d1921d5abdda7405f7f980d4e98f1d79
daniel <daniel@roverpass.com>
2025-07-11 01:45:00 +0200

    staticfiles folder

--- premium_websites/settings.py
+++ premium_websites/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"), # Django will search for /src/
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
